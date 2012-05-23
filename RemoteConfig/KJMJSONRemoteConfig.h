b11d47acc87a42cc09643d80001d1bc04cb99a9e
Kevin Renskers <info@bolhoed.net>
2012-05-23 18:08:44 +0000

    Initial commit, still a work in progress

---
+++ RemoteConfig/KJMJSONRemoteConfig.h
@@ -0,0 +1,13 @@
+//
+//  JSONRemoteConfig.h
+//  metrolyrics
+//
+//  Created by Kevin Renskers on 23-05-12.
+//  Copyright (c) 2012 Gangverk. All rights reserved.
+//
+
+#import "KJMBaseRemoteConfig.h"
+
+@interface KJMJSONRemoteConfig : KJMBaseRemoteConfig
+
+@end
---
+++ RemoteConfig/KJMXMLRemoteConfig.h
@@ -0,0 +1,13 @@
+//
+//  KJMXMLRemoteConfig.h
+//  metrolyrics
+//
+//  Created by Kevin Renskers on 23-05-12.
+//  Copyright (c) 2012 Gangverk. All rights reserved.
+//
+
+#import "KJMBaseRemoteConfig.h"
+
+@interface KJMXMLRemoteConfig : KJMBaseRemoteConfig
+
+@end
